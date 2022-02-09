08ef4380390d4f5d266c154eb2dd0745a86b3d74
s0loparty <izovo@bk.ru>
2022-02-09 10:50:59 +0300

    picture attrs

--- index.js
+++ index.js
@@ -21,19 +21,36 @@ form.addEventListener('submit', ev => {
 		console.log('domElement: ', domElement);
 
 		const src = domElement.getAttribute('src')
-		const classes = domElement.getAttribute('class') ? `class="${domElement.getAttribute('class')}"` : ''
-		const style = domElement.getAttribute('style') ? `style="${domElement.getAttribute('style')}"` : ''
-		const w = domElement.getAttribute('width') ? `width="${domElement.getAttribute('width')}"` : ''
-		const h = domElement.getAttribute('height') ? `height="${domElement.getAttribute('height')}"` : ''
-		const alt = domElement.getAttribute('alt') ? `alt="${domElement.getAttribute('alt')}"` : ''
+		// const classes = domElement.getAttribute('class') ? `class="${domElement.getAttribute('class')}"` : ''
+		// const style = domElement.getAttribute('style') ? `style="${domElement.getAttribute('style')}"` : ''
+		// const w = domElement.getAttribute('width') ? `width="${domElement.getAttribute('width')}"` : ''
+		// const h = domElement.getAttribute('height') ? `height="${domElement.getAttribute('height')}"` : ''
+		// const alt = domElement.getAttribute('alt') ? `alt="${domElement.getAttribute('alt')}"` : ''
+
+		let stringAttrs = ''
+		const attrs = [
+			{el: 'class', value: domElement.getAttribute('class')},
+			{el: 'style', value: domElement.getAttribute('style')},
+			{el: 'width', value: domElement.getAttribute('width')},
+			{el: 'height', value: domElement.getAttribute('height')},
+			{el: 'alt', value: domElement.getAttribute('alt')}
+		]
+
+		attrs.forEach(item => {
+			if (item.value !== null && item.value.length && item.value.indexOf('wheel-img') === -1) {
+				pic.setAttribute(item.el, item.value)
+
+				stringAttrs += `${item.el}="${item.value}" `
+			}
+		})
 
 		const typeLength = src.split('.').pop().length + 1
 		const webpSrc = inputValue.length 
 			? `${inputValue}/${src.slice(0, -typeLength).split('/').pop()}.webp` 
 			: `${src.slice(0, -typeLength)}.webp`
-		console.log(webpSrc);
+		console.log('webp src:', webpSrc);
 		
-		pic.innerHTML = `<source srcset="${webpSrc}" type="image/webp" ${classes} ${style} ${w} ${h} ${alt}>`
+		pic.innerHTML = `<source srcset="${webpSrc}" type="image/webp" ${stringAttrs}>`
 		pic.innerHTML += img
 
 		__temp_index_content = __temp_index_content.replaceAll(img, pic.outerHTML)
