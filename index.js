b071a260982f6f15f8f7c628b36f973216ccd619
s0loparty <izovo@bk.ru>
2022-02-10 17:32:34 +0300

    fix match picture

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', ev => {
 
 	const indexContent = ev.target[0].value
 	const allImages = indexContent.match(/<img(.*?)([\s\S]*?)>/g)
-	const allPictures = indexContent.match(/<picture(.*?)([\s\S]*?)><\/picture>/g)
+	const allPictures = indexContent.match(/<picture(.*?)([\s\S]*?)>([\s\S]*?)<\/picture>/g)
 
 	let __temp_index_content = indexContent
 
@@ -18,64 +18,66 @@ form.addEventListener('submit', ev => {
 	if (allPictures) alert('Осторожно! В шаблоне уже есть <picture>!')
 
 	// добавляем data-meowmeow ко всем img внутри всех существующих picture
-	// if (allPictures) {
-	// 	allPictures.forEach(pic => {
-	// 		const elementPic = strToDom(pic, 'picture')
-	// 		elementPic.querySelector('img').dataset.meowmeow = 'true'
+	if (allPictures) {
+		allPictures.forEach(pic => {
+			const elementPic = strToDom(pic, 'picture')
+			elementPic.querySelector('img').dataset.meowmeow = 'true'
 
-	// 		__temp_index_content = __temp_index_content.replaceAll(pic, elementPic.outerHTML)
-	// 	})
-	// }
+			__temp_index_content = __temp_index_content.replaceAll(pic, elementPic.outerHTML)
+		})
+	}
 
-	allImages.some(img => {
-		const pic = document.createElement('picture')
-		const domElement = strToDom(img)
+	setTimeout(() => {
+		allImages.some(img => {
+			const pic = document.createElement('picture')
+			const domElement = strToDom(img)
 
-		if (!domElement || domElement.dataset.meowmeow) return
-		if (
-			domElement.src.split('.').pop().indexOf('svg') !== -1 || 
-			domElement.src.split('.').pop().indexOf('gif') !== -1
-		) return
+			if (!domElement || domElement.dataset.meowmeow) return
+			if (
+				domElement.src.split('.').pop().indexOf('svg') !== -1 || 
+				domElement.src.split('.').pop().indexOf('gif') !== -1
+			) return
 
-		console.log('domElement: ', domElement);
+			console.log('domElement: ', domElement);
 
-		domElement.dataset.meowmeow = true
+			domElement.dataset.meowmeow = true
 
-		const src = domElement.getAttribute('src')
-		let stringAttrs = ''
-		const attrs = [
-			{el: 'class', value: domElement.getAttribute('class')},
-			{el: 'style', value: domElement.getAttribute('style')},
-			{el: 'width', value: domElement.getAttribute('width')},
-			{el: 'height', value: domElement.getAttribute('height')},
-			{el: 'alt', value: domElement.getAttribute('alt')}
-		]
+			const src = domElement.getAttribute('src')
+			let stringAttrs = ''
+			const attrs = [
+				{el: 'class', value: domElement.getAttribute('class')},
+				{el: 'style', value: domElement.getAttribute('style')},
+				{el: 'width', value: domElement.getAttribute('width')},
+				{el: 'height', value: domElement.getAttribute('height')},
+				{el: 'alt', value: domElement.getAttribute('alt')}
+			]
 
-		attrs.forEach(item => {
-			if (item.value !== null && item.value.length && item.value.indexOf('wheel-img') === -1) {
-				pic.setAttribute(item.el, item.value)
+			attrs.forEach(item => {
+				if (item.value !== null && item.value.length && item.value.indexOf('wheel-img') === -1) {
+					pic.setAttribute(item.el, item.value)
 
-				stringAttrs += `${item.el}="${item.value}" `
-			}
+					stringAttrs += `${item.el}="${item.value}" `
+				}
+			})
+
+			const typeLength = src.split('.').pop().length + 1
+			const webpSrc = inputValue.length 
+				? `${inputValue}/${src.slice(0, -typeLength).split('/').pop()}.webp` 
+				: `${src.slice(0, -typeLength)}.webp`
+
+			console.log('webp src:', webpSrc)
+			
+			pic.innerHTML = `<source srcset="${webpSrc}" type="image/webp" ${stringAttrs}>`
+			pic.innerHTML += domElement.outerHTML
+
+			__temp_index_content = __temp_index_content.replaceAll(img, pic.outerHTML)
+
+			console.log(pic);
+			console.log('=================');
 		})
 
-		const typeLength = src.split('.').pop().length + 1
-		const webpSrc = inputValue.length 
-			? `${inputValue}/${src.slice(0, -typeLength).split('/').pop()}.webp` 
-			: `${src.slice(0, -typeLength)}.webp`
-
-		console.log('webp src:', webpSrc)
-		
-		pic.innerHTML = `<source srcset="${webpSrc}" type="image/webp" ${stringAttrs}>`
-		pic.innerHTML += domElement.outerHTML
-
-		__temp_index_content = __temp_index_content.replaceAll(img, pic.outerHTML)
-
-		console.log(pic);
-		console.log('=================');
-	})
-
-	res.value = __temp_index_content
+		res.value = __temp_index_content
+	}, 0)
 })
 
 input.addEventListener('input', ({ target }) => inputValue = target.value)
